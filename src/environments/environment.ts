010c275aacff35505bd1a448aa06dad35d0b2f20
mhdb96 <mhd.b96@gmail.com>
2020-04-19 05:17:37 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  googleMapsAPIKey: "your_api",
-  fireBaseAPIKey: "your_api",
+  googleMapsAPIKey: "AIzaSyAlq_pwb-kfuBrOQbD68Gt6De61NPx7zD8",
+  fireBaseAPIKey: "AIzaSyAlq_pwb-kfuBrOQbD68Gt6De61NPx7zD8",
 };
 
 /*
